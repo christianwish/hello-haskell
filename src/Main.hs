9ae3769fd78af2d1570dc05ad6ddef3b5eb11adc
Konstantin Silin <konstantin.silin@gmail.com>
2018-08-20 20:17:44 +0200

    stack.yaml, formatting, dirs

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,18 @@
 module Main where
 
-import Control.Monad
-import Happstack.Server (nullConf, simpleHTTP, ok, dir)
-import System.IO
-import System.Log.Handler.Simple
-import System.Log.Logger ( updateGlobalLogger
-                         , rootLoggerName
-                         , setLevel
-                         , Priority(..)
-                         )
+import           Control.Monad
+import           Happstack.Server          (dir, dirs, nullConf, ok, simpleHTTP)
+import           System.IO
+import           System.Log.Handler.Simple
+import           System.Log.Logger         (Priority (..), rootLoggerName,
+                                            setLevel, updateGlobalLogger)
 
-endPoints = [
-        dir "hello" $ dir "exellio" $ ok "Hello Exellio!"
-      , dir "hello" $ ok "Hello!"
-      , ok "hello world!\ntry http://localhost:8000/hello\nor http://localhost:8000/hello/exellio"
-      ]
+endPoints =
+  [ mzero
+  , dirs "hello/exellio" $ ok "Hello Exellio!"
+  , dir "hello" $ ok "Hello!"
+  , ok "hello world!\ntry http://localhost:8000/hello\nor http://localhost:8000/hello/exellio"
+  ]
 
 main :: IO ()
 main = do
